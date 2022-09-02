cdb0fd68dd7f942e9c623a62e86cce86a7619dd7
huige <61108540+huige233@users.noreply.github.com>
2022-09-02 22:38:11 +0800

    Update CommonProxy.java

--- src/main/java/huige233/transcend/proxy/CommonProxy.java
+++ src/main/java/huige233/transcend/proxy/CommonProxy.java
@@ -6,6 +6,7 @@ import huige233.transcend.compat.tinkers.TiCConfig;
 import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class CommonProxy {
@@ -16,16 +17,22 @@ public class CommonProxy {
 
     public void preInit( FMLPreInitializationEvent event )
     {
+        if(Loader.isModLoaded("tconstruct")){
+            TiCConfig.setup();
+            TiCConfig.setRenderInfo();
+            //MinecraftForge.EVENT_BUS.register(TiCConfig.setup());
+        }
+    }
+
+
+    public void init( FMLInitializationEvent event )
+    {
         if(Loader.isModLoaded("psi")){
             MinecraftForge.EVENT_BUS.register(PsiCompat.class);
         }
         if(Loader.isModLoaded("avartia")) {
             Avartiabreak.enabled = true;
         }
-        if(Loader.isModLoaded("tconstruct")){
-            TiCConfig.setup();
-            TiCConfig.setRenderInfo();
-            //MinecraftForge.EVENT_BUS.register(TiCConfig.setup());
-        }
+       
     }
 }
