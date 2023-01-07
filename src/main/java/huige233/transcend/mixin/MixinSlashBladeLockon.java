ff176e3e9873a8d461916401adbdddb1c5181c6f
dbydd <1992003927@qq.com>
2023-01-07 17:21:42 +0800

    modified slashblade

--- src/main/java/huige233/transcend/mixin/MixinSlashBladeLockon.java
+++ src/main/java/huige233/transcend/mixin/MixinSlashBladeLockon.java
@@ -1,21 +1,34 @@
 package huige233.transcend.mixin;
 
 import huige233.transcend.util.ArmorUtils;
+import mods.flammpfeil.slashblade.TagPropertyAccessor;
 import mods.flammpfeil.slashblade.item.ItemSlashBlade;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ItemSlashBlade.class)
+@Mixin(value = ItemSlashBlade.class,remap = false)
 public class MixinSlashBladeLockon {
 
-    @Inject(method = "faceEntity", at = @At("HEAD"), cancellable = true)
-    private static void InjectorFaceEntity(EntityLivingBase owner, Entity par1Entity, float par2, float par3, CallbackInfo ci) {
+    @Shadow(remap = false)
+    public static TagPropertyAccessor.TagPropertyInteger TargetEntityId;
+
+//    @Inject(method = "func_77663_a",
+//            at = @At(value = "INVOKE", target = "Lmods/flammpfeil/slashblade/item/ItemSlashBlade;faceEntity(Lnet/minecraft/entity/EntityLivingBase;Lnet/minecraft/entity/Entity;FF)V", shift = At.Shift.AFTER),
+//            locals = LocalCapture.PRINT, cancellable = true)
+//    private static void InjectorLockOn(ItemStack sitem, World par2World, Entity par3Entity, int indexOfMainSlot, boolean isCurrent, CallbackInfo ci) {
+////        TargetEntityId.set(tag, 0);
+//    }
+
+    @Inject(method = "faceEntity", at = @At("HEAD"), cancellable = true,remap = false)
+    private static void InjectFaceEntity(EntityLivingBase owner, Entity par1Entity, float par2, float par3, CallbackInfo ci) {
         if (par1Entity instanceof EntityPlayer && ArmorUtils.fullEquipped((EntityPlayer) par1Entity)) {
+            TargetEntityId.set(ItemSlashBlade.getItemTagCompound(owner.getHeldItemMainhand()), 0);
             ci.cancel();
         }
     }
