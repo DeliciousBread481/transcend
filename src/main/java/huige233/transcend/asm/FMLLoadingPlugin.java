2308baf6d66f4e4b26c8841755ee7d2d9025bca2
huige233 <huige1463@gmail.com>
2025-10-13 14:30:09 +0800

    Update FMLLoadingPlugin.java

--- src/main/java/huige233/transcend/asm/FMLLoadingPlugin.java
+++ src/main/java/huige233/transcend/asm/FMLLoadingPlugin.java
@@ -1,12 +1,11 @@
 package huige233.transcend.asm;
 
-import net.minecraft.launchwrapper.Launch;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import net.minecraft.launchwrapper.Launch;
 
 import javax.annotation.Nullable;
 import java.lang.reflect.Method;
 import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.List;
 import java.util.Map;
 
@@ -16,40 +15,48 @@ import java.util.Map;
 public class FMLLoadingPlugin implements IFMLLoadingPlugin {
 
     static {
-        addTweaker();
-    }
-
-    private static void addTweaker() {
         try {
-            List<String> classes = (List<String>) Launch.blackboard.get("TweakClasses");
-            classes.add("huige233.transcend.asm.tweaker.Tweaker");
-
-            URL url = FMLLoadingPlugin.class.getProtectionDomain().getCodeSource().getLocation();
-
-            try {
-                Method addURLMethod = Launch.classLoader.getClass().getDeclaredMethod("addURL", URL.class);
-                addURLMethod.setAccessible(true);
-                addURLMethod.invoke(Launch.classLoader, url);
-            } catch (NoSuchMethodException | IllegalAccessException e) {
-                try {
-                    Method addURLMethod2 = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
-                    addURLMethod2.setAccessible(true);
-                    ClassLoader sysLoader = ClassLoader.getSystemClassLoader();
-                    if (addURLMethod2.getDeclaringClass().isInstance(sysLoader)) {
-                        addURLMethod2.invoke(sysLoader, url);
-                    }
-                } catch (Throwable t) {
-                    t.printStackTrace();
-                }
-            }
+            addTweaker();
         } catch (Throwable t) {
-            throw new RuntimeException("Failed to add Tweaker", t);
+            System.err.println("[Transcend] Failed to add Tweaker: " + t);
+            t.printStackTrace();
         }
     }
 
+    private static void addTweaker() throws Exception {
+        List<String> classes = (List<String>) Launch.blackboard.get("TweakClasses");
+        if (!classes.contains("huige233.transcend.asm.tweaker.Tweaker")) {
+            classes.add("huige233.transcend.asm.tweaker.Tweaker");
+            System.out.println("[Transcend] Tweaker registered: huige233.transcend.asm.tweaker.Tweaker");
+        }
+
+        URL url = FMLLoadingPlugin.class.getProtectionDomain().getCodeSource().getLocation();
+        ClassLoader cl = Launch.classLoader;
+
+        try {
+            Method addURL = cl.getClass().getDeclaredMethod("addURL", URL.class);
+            addURL.setAccessible(true);
+            addURL.invoke(cl, url);
+            System.out.println("[Transcend] CoreMod JAR injected into LaunchClassLoader.");
+        } catch (Throwable t) {
+            System.err.println("[Transcend] Failed to inject via LaunchClassLoader, fallback to system loader.");
+            Method addURL2 = java.net.URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+            addURL2.setAccessible(true);
+            addURL2.invoke(ClassLoader.getSystemClassLoader(), url);
+        }
+
+        try {
+            Class<?> tweakerClass = Class.forName("huige233.transcend.asm.tweaker.Tweaker", true, cl);
+            System.out.println("[Transcend] Tweaker class preloaded successfully: " + tweakerClass);
+        } catch (Throwable preloadErr) {
+            System.err.println("[Transcend] Failed to preload Tweaker class: " + preloadErr);
+        }
+    }
+
+
     @Override
     public String[] getASMTransformerClass() {
-        return new String[]{"huige233.transcend.asm.ClassTransformer"};
+        return new String[] { "huige233.transcend.asm.ClassTransformer" };
     }
 
     @Override
@@ -65,7 +72,9 @@ public class FMLLoadingPlugin implements IFMLLoadingPlugin {
 
     @Override
     public void injectData(Map<String, Object> data) {
-        RemapUtils.isDevelopmentEnvironment = !(boolean) data.get("runtimeDeobfuscationEnabled");
+        // 环境标记
+        boolean runtimeDeobf = (boolean) data.get("runtimeDeobfuscationEnabled");
+        huige233.transcend.asm.RemapUtils.isDevelopmentEnvironment = !runtimeDeobf;
     }
 
     @Override
